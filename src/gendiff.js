b460789cfa1e3dccb1a918d975dc928ab25d3d5d
javazee <edza92@mail.ru>
2024-05-14 21:32:10 +0500

    added option for format

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,15 +1,5 @@
-import { Command } from 'commander';
-
 const gendiff = (file1, file2) => {
-    const program = new Command();
-
-    program
-        .name('gendiff')
-        .description('Compares two configuration files and shows a difference')
-        .version('1.0.0');
-
-    program.parse();
-    console.log('No differences for a while. Code is in development');
+    //
 };
 
 export {
